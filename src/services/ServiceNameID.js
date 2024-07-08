8888bdb5c37f2e24da51300ee09004811e5fcb9e
vitorYghor <devictoryghor@gmail.com>
2024-07-08 17:26:25 -0300

    style: using JSDoc for functions

--- src/services/ServiceNameID.js
+++ src/services/ServiceNameID.js
@@ -1,27 +1,43 @@
+/**
+ * Função para converter IDs de atribuidores em nomes de usuários
+ * @param {*} assignerIds 
+ * @param {*} storedUsers 
+ * @returns o nome do usuário ou o ID se o nome não for encontrado
+ */
 function convertAssignerIdsToNames(assignerIds, storedUsers) {
-    // Função para converter IDs de atribuidores em nomes de usuários
     return assignerIds.split(', ').map(id => {
         const user = storedUsers.find(user => user.id.toString() === id.toString());
-        return user ? user.nome : `ID: ${id}`; // Retorna o nome do usuário ou o ID se o nome não for encontrado
+        return user ? user.nome : `ID: ${id}`; 
     }).join(', '); // Junta os nomes separados por ', '
 }
 
+/**
+ * Função para obter os nomes dos atribuidores a partir dos IDs
+ * @param {*} assignerIds 
+ * @param {*} storedUsers 
+ * @returns o nome do usuário ou o ID se o nome não for encontrado Junta os nomes separados por ', '
+ */
 function getAssignerNames(assignerIds, storedUsers) {
-    // Função para obter os nomes dos atribuidores a partir dos IDs
     return assignerIds.map(id => {
         const user = storedUsers.find(user => user.id.toString() === id.toString());
-        return user ? user.nome : `ID: ${id}`; // Retorna o nome do usuário ou o ID se o nome não for encontrado
-    }).join(', '); // Junta os nomes separados por ', '
+        return user ? user.nome : `ID: ${id}`; 
+    }).join(', ');
 }
 
+/**
+ *  Função para obter os IDs dos atribuidores a partir dos nomes
+ * 
+ * @param {*} assignerNames 
+ * @param {*} storedUsers 
+ * @returns null se o nome do usuário não for encontrado nos usuários armazenados
+ */
 function getAssignerIds(assignerNames, storedUsers) {
-    // Função para obter os IDs dos atribuidores a partir dos nomes
     const ids = assignerNames.map(name => {
         const user = storedUsers.find(user => user.nome === name.trim());
         if (user) {
             return user.id;
         } else {
-            return null; // Retorna null se o nome do usuário não for encontrado nos usuários armazenados
+            return null; 
         }
     }).filter(id => id !== null).join(', '); // Remove qualquer valor null e junta os IDs com ', '
 
