9bf1057c0d59aba6e7c5bd57f71f3b75239aaeea
vitorYghor <devictoryghor@gmail.com>
2024-07-19 17:05:59 -0300

    fix: define is not a function

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+const sequelize = require('../util/Database');
+// Import the built-in data types
+const { DataTypes } = require('sequelize');
+
+const User = sequelize.define(
+    'User',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            unique: true
+        },
+        name_eva: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        id_eva: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            
+        }
+    }
+);
+
+module.exports = { User };
