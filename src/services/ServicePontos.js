dbdc514a4bdc2217f8fc9f870763e9fde9a1a17c
Otoniel <79499426+otonielnn@users.noreply.github.com>
2024-07-23 18:50:46 +0000

    fixing negototiate case in calcularPontosEva
    Signed-off-by: Otoniel <79499426+otonielnn@users.noreply.github.com>

--- src/services/ServicePontos.js
+++ src/services/ServicePontos.js
@@ -7,33 +7,37 @@ const { extractNegotiationModel } = require("./ServiceDescription");
  * @returns
  */
 function calcularPontosEVA(task, user) {
-  const negotiateTitleRegex = /\[(G|I|N):\s*(\d+)\s*x\s*(\d+(?:\.\d+)?)\]/i;
-  const match = task.titulo.match(negotiateTitleRegex);
+    const negotiateTitleRegex = /\[(G|I|N):\s*(\d+)\s*x\s*(\d+(?:\.\d+)?)\]/i;
+    const match = task.titulo.match(negotiateTitleRegex);
 
-  if (match) {
-    const pontos = parseInt(match[2], 10); // Extrai o número de pontos do título
-    const multiplicador = parseFloat(match[3]); // Extrai o multiplicador do título
+    if (match) {
+        const pontos = parseInt(match[2], 10); // Extrai o número de pontos do título
+        const multiplicador = parseFloat(match[3]); // Extrai o multiplicador do título
 
-    switch (match[1].toUpperCase()) {
-      case "G":
-        return pontos * multiplicador; // Exemplo: Tarefa do tipo 'G' vale 8 pontos EVA
-      case "I":
-        return pontos * multiplicador; // Exemplo: Tarefa do tipo 'I' vale 4 pontos EVA
-      case "N":
-        try {
-        const negotiationModel = extractNegotiationModel(task.descricao);
-        consoleOccinho?.log("points = ", getNegotiationsPoints(negotiationModel, user.nome));
-        const points = getNegotiationsPoints(negotiationModel, user.nome);
-        return points; // Exemplo: Tarefa do tipo 'N' vale 2 pontos EVA
-        } catch(error) {
-            console.error(error)
+        switch (match[1].toUpperCase()) {
+            case "G":
+                return pontos * multiplicador; // Exemplo: Tarefa do tipo 'G' vale 8 pontos EVA
+            case "I":
+                return pontos * multiplicador; // Exemplo: Tarefa do tipo 'I' vale 4 pontos EVA
+            case "N":
+                try {
+                    const negotiationModel = extractNegotiationModel(task.descricao);
+                    if (negotiationModel !== '') {
+                        const points = getNegotiationsPoints(negotiationModel, user.nome);
+                        consoleOccinho?.log("points = ", points);
+                        return points; // Exemplo: Tarefa do tipo 'N' vale 2 pontos EVA   
+                    } else {
+                        console.log(`A Tarefa: ${task.titulo} ainda não foi negociada!!!`);
+                    }
+                } catch (error) {
+                    console.error(error)
+                }
+            default:
+                return 0; // Retorna 0 pontos EVA se não houver correspondência
         }
-      default:
-        return 0; // Retorna 0 pontos EVA se não houver correspondência
     }
-  }
 
-  return 0; // Retorna 0 pontos EVA se não encontrar correspondência
+    return 0; // Retorna 0 pontos EVA se não encontrar correspondência
 }
 
 /**
@@ -44,21 +48,21 @@ function calcularPontosEVA(task, user) {
  */
 function getNegotiationsPoints(model, evaUserName) {
     consoleOccinho?.log("model =", model);
-  const usersAndPoints = model.split(",");
-  const regexFullNameEva = /[a-zA-Z\u00C0-\u017F]+( [a-zA-Z\u00C0-\u017F]+)+/;
-  const regexDigit = /\d+/
-  for (const userAndPoints of usersAndPoints) {
-    consoleOccinho?.log(userAndPoints)
-    const name = userAndPoints.split(":")[1].match(regexFullNameEva)[0];
+    const usersAndPoints = model.split(",");
+    const regexFullNameEva = /[a-zA-Z\u00C0-\u017F]+( [a-zA-Z\u00C0-\u017F]+)+/;
+    const regexDigit = /\d+/
+    for (const userAndPoints of usersAndPoints) {
+        consoleOccinho?.log(userAndPoints)
+        const name = userAndPoints.split(":")[1].match(regexFullNameEva)[0];
 
-    if(name.toLowerCase() == evaUserName.toLowerCase()) {
-        const points = userAndPoints.split(":")[0].match(regexDigit)[0];
-        return points
+        if (name.toLowerCase() == evaUserName.toLowerCase()) {
+            const points = userAndPoints.split(":")[0].match(regexDigit)[0];
+            return points
+        }
     }
-  }
 
-  throw Error(`Utilizando o model = ${model} e com o nome de usuário ${evaUserName} não foi possível atribuir pontos de negociação`)
-  return 0 ;
+    throw Error(`Utilizando o model = ${model} e com o nome de usuário ${evaUserName} não foi possível atribuir pontos de negociação`)
+    return 0;
 }
 
 /**
@@ -69,23 +73,23 @@ function getNegotiationsPoints(model, evaUserName) {
  * @returns
  */
 function calcularPontosXP(
-  pontosEVA,
-  mediaPontosEVA,
-  numeroSprintsParticipadas
+    pontosEVA,
+    mediaPontosEVA,
+    numeroSprintsParticipadas
 ) {
-  // Calcula pontos XP com base nos parâmetros fornecidos
-  if (numeroSprintsParticipadas >= 3 && mediaPontosEVA >= 16) {
-    return pontosEVA * 30; // Se participou de 3 ou mais sprints e média de pontos EVA >= 16
-  } else if (numeroSprintsParticipadas >= 3 && mediaPontosEVA < 16) {
-    // Fórmula modificada para médiaPontosEVA < 16
-    return (16 - mediaPontosEVA) ** 1 * -pontosEVA + pontosEVA * 30;
-  } else {
-    // Primeira e segunda sprint, 1 pontoEVA equivale a 30 XP
-    return pontosEVA * 30;
-  }
+    // Calcula pontos XP com base nos parâmetros fornecidos
+    if (numeroSprintsParticipadas >= 3 && mediaPontosEVA >= 16) {
+        return pontosEVA * 30; // Se participou de 3 ou mais sprints e média de pontos EVA >= 16
+    } else if (numeroSprintsParticipadas >= 3 && mediaPontosEVA < 16) {
+        // Fórmula modificada para médiaPontosEVA < 16
+        return (16 - mediaPontosEVA) ** 1 * -pontosEVA + pontosEVA * 30;
+    } else {
+        // Primeira e segunda sprint, 1 pontoEVA equivale a 30 XP
+        return pontosEVA * 30;
+    }
 }
 
 module.exports = {
-  calcularPontosEVA,
-  calcularPontosXP,
+    calcularPontosEVA,
+    calcularPontosXP,
 };
